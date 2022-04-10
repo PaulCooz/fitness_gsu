410d1d925576bb57c46b9d82c16c9351f34272c7
Paul <steam.gish@mail.ru>
2022-04-10 22:39:32 +0300

    graph view correction

--- Assets/Scripts/Views/GraphView.cs
+++ Assets/Scripts/Views/GraphView.cs
@@ -21,31 +21,76 @@ namespace Views
 
         public IEnumerator Show(List<Parameter.ValueAndDate> values)
         {
-            var valueTexts = new TextMeshProUGUI[values.Count];
-            var dateTexts = new TextMeshProUGUI[values.Count];
-            var maxValue = float.MinValue;
+            var uniqValues = GetUniqValues(values);
+            var dateTexts = GetDataTexts(values);
+            var valueTexts = GetValueTexts(uniqValues);
 
-            for (var i = 0; i < values.Count; i++)
-            {
-                valueTexts[i] = Instantiate(textPrefab, leftAxis);
-                dateTexts[i] = Instantiate(textPrefab, bottomAxis);
+            var waitForAnimEnd = new WaitForSeconds(0.5f);
+            yield return waitForAnimEnd;
 
-                valueTexts[i].text = values[i].Value.ToString(CultureInfo.CurrentCulture);
-                dateTexts[i].text = values[i].DateTime.ToString(CultureInfo.CurrentCulture);
-
-                maxValue = Math.Max(maxValue, values[i].Value);
-            }
-
-            yield return new WaitForSeconds(0.5f);
-
-            var positions = new Vector3[values.Count];
-            for (var i = 0; i < values.Count; i++)
-            {
-                positions[i] = new Vector3(valueTexts[i].transform.position.y, values[i].Value / maxValue, 0);
-            }
+            var positions = GetPositions(values, valueTexts, dateTexts);
 
             lineRenderer.positionCount = positions.Length;
             lineRenderer.SetPositions(positions);
+        }
+
+        private static Vector3[] GetPositions(IReadOnlyList<Parameter.ValueAndDate> values,
+            IReadOnlyList<TextMeshProUGUI> valueTexts, IReadOnlyList<TextMeshProUGUI> dateTexts)
+        {
+            var positions = new Vector3[values.Count];
+            for (var i = 0; i < values.Count; i++)
+            {
+                var k = 0;
+                for (var j = 0; j < valueTexts.Count; j++)
+                {
+                    if (valueTexts[j].text != values[i].Value.ToString(CultureInfo.CurrentCulture)) continue;
+
+                    k = j;
+                    break;
+                }
+
+                positions[i] = new Vector3(dateTexts[i].transform.position.x, valueTexts[k].transform.position.y, 0);
+            }
+
+            return positions;
+        }
+
+        private TextMeshProUGUI[] GetValueTexts(IReadOnlyList<float> uniqValues)
+        {
+            var valueTexts = new TextMeshProUGUI[uniqValues.Count];
+            for (var i = 0; i < uniqValues.Count; i++)
+            {
+                valueTexts[i] = Instantiate(textPrefab, leftAxis);
+                valueTexts[i].text = uniqValues[i].ToString(CultureInfo.CurrentCulture);
+            }
+
+            return valueTexts;
+        }
+
+        private TextMeshProUGUI[] GetDataTexts(IReadOnlyList<Parameter.ValueAndDate> values)
+        {
+            var dateTexts = new TextMeshProUGUI[values.Count];
+            for (var i = 0; i < values.Count; i++)
+            {
+                dateTexts[i] = Instantiate(textPrefab, bottomAxis);
+                dateTexts[i].text = values[i].DateTime.ToString(CultureInfo.CurrentCulture);
+            }
+
+            return dateTexts;
+        }
+
+        private static List<float> GetUniqValues(IReadOnlyList<Parameter.ValueAndDate> values)
+        {
+            var uniqValues = new List<float>();
+            for (var i = 0; i < values.Count; i++)
+            {
+                if (uniqValues.Contains(values[i].Value)) continue;
+
+                uniqValues.Add(values[i].Value);
+            }
+
+            uniqValues.Sort((f1, f2) => f2.CompareTo(f1));
+            return uniqValues;
         }
     }
 }
